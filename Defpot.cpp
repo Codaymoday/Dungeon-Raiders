e12a02d47cca10cf673e93cf5a653fab551ce77c
TKK-NYP <251214x@mymail.nyp.edu.sg>
2026-08-27 16:24:01 +0800

    Delete Defpot.cpp

--- Defpot.cpp
+++
@@ -1,7 +0,0 @@
-#include "Defpot.h"
-Defpot::Defpot()
-{
-	hp = 5, atk = 0, price = rand() % 10 + 25, def = 10, speedreduction = 0;
-	name = "Defence Potion";
-	equipable = false, consumable = true, mageequipable = false;
-}
